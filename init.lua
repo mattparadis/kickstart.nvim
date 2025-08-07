9ae07f398a43508e56080d86cf66854e3b2b8ca7
mattparadis <mattia.paradiso1611@gmail.com>
2025-08-07 12:19:49 +0200

    aa

--- init.lua
+++ init.lua
@@ -503,5 +503,9 @@ end,
   },
 })
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+vim.opt.background = "dark"
+vim.cmd("colorscheme oxocarbon")
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
--- lua/custom/plugins/coloschemes.lua
+++ lua/custom/plugins/coloschemes.lua
@@ -1,8 +1,15 @@
 return {
-  { "morhetz/gruvbox" },
-  { "ellisonleao/gruvbox.nvim" },
-  { "navarasu/onedark.nvim" },
-  { "rebelot/kanagawa.nvim" },
-  { "catppuccin/nvim", name = "catppuccin" },
-  { "folke/tokyonight.nvim" },
+  { 'nyoom-engineering/oxocarbon.nvim' },
+  { 'morhetz/gruvbox' },
+  { 'ellisonleao/gruvbox.nvim' },
+  { 'navarasu/onedark.nvim' },
+  { 'rebelot/kanagawa.nvim' },
+  { 'catppuccin/nvim', name = 'catppuccin' },
+  { 'folke/tokyonight.nvim' },
+  {
+    'cpplain/flexoki.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
 }
